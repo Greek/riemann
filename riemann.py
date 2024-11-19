1932525a1eb0d01a9622d243f8d9ff193716ad83
Greek <apapuig@gmail.com>
2024-11-19 13:24:31 -0500

    [WIP] initial riemann script

---
+++ riemann.py
@@ -0,0 +1,13 @@
+def main():
+    
+    # a
+    lower = 0
+    # b
+    upper = 2
+    # n
+    subintervals = 4
+
+    width = (upper - lower) / subintervals
+
+    for i in range(subintervals):
+
