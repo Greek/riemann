7e09dd6888dc9b4e88d823372ab6e23c17699370
Andreas <git@fsck.rip>
2024-11-19 19:17:49 -0500

    finish
    Signed-off-by: Andreas <git@fsck.rip>

--- riemann.py
+++ riemann.py
@@ -1,13 +1,34 @@
 def main():
-    
-    # a
-    lower = 0
-    # b
-    upper = 2
-    # n
-    subintervals = 4
+    # a = lower, b = upper
+    lower, upper = 3, 6
 
+    # subintervals = n
+    subintervals = 3
+
+    # ∆x (delta x)
     width = (upper - lower) / subintervals
 
-    for i in range(subintervals):
+    # resulting sums
+    left_sum, right_sum = 0, 0
 
+    # the function we want to integrate (should be user input)
+    def function(x: int):
+        return (4*x**2+3*x+2)
+
+    # calculate the sum of the left-endpoints by i
+    # where we count from `a` up to `n`
+    for i in range(subintervals):
+        left_sum += function(lower + i * width) * width
+
+    # calculate the sum of the right-endpoints by i
+    # where we count from `1` up to `n + 1`
+    for i in range(1, subintervals + 1):
+        right_sum += function(lower + i * width) * width
+
+    return left_sum, right_sum
+
+
+if __name__ == "__main__":
+    result = main()
+    print("left\tright")
+    print(result)
