4e1c7b9a884f1396db01808978d182b0a5172e8f
Andreas <git@fsck.rip>
2024-11-21 10:56:35 -0500

    make it more mathematical
    Signed-off-by: Andreas <git@fsck.rip>

--- riemann.py
+++ riemann.py
@@ -1,12 +1,12 @@
 def main():
-    # a = lower, b = upper
-    lower, upper = 3, 6
+    # a: lower, b: upper
+    a, b = 3, 6
 
-    # subintervals = n
-    subintervals = 3
+    # n: subintervals
+    n = 3
 
     # ∆x (delta x)
-    width = (upper - lower) / subintervals
+    width = (b - a) / n
 
     # resulting sums
     left_sum, right_sum = 0, 0
@@ -15,15 +15,15 @@ def main():
     def function(x: int):
         return (4*x**2+3*x+2)
 
-    # calculate the sum of the left-endpoints by i
-    # where we count from `a` up to `n`
-    for i in range(subintervals):
-        left_sum += function(lower + i * width) * width
+    # left riemann
+    # start from a zero-indexed loop up to `n`
+    for i in range(n):
+        left_sum += function(a + i * width) * width
 
-    # calculate the sum of the right-endpoints by i
-    # where we count from `1` up to `n + 1`
-    for i in range(1, subintervals + 1):
-        right_sum += function(lower + i * width) * width
+    # right riemann
+    # start from 1 up to `n+1` to avoid zero-index
+    for i in range(1, n + 1):
+        right_sum += function(a + i * width) * width
 
     return left_sum, right_sum
 
